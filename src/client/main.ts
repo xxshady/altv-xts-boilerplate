056afda13c2e468fa5102114a8c19053efed820f
xshady <fastik036@gmail.com>
2022-12-11 17:55:14 +0300

    add meta example to client too

--- src/client/main.ts
+++ src/client/main.ts
@@ -2,3 +2,8 @@ import alt from "alt-client"
 import { SOMETHING } from "@shared/main"
 
 alt.log("hello world", SOMETHING)
+
+const player = alt.Player.getByID(-1)
+// number | undefined too, see ICustomPlayerStreamSyncedMeta
+const playerValue = player?.getStreamSyncedMeta("numberExample")
+alt.log("playerValue:", playerValue) // undefined
