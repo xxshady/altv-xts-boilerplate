ebfa09dba60a416dfeffe99265f4fa9e417cec9b
xshady <fastik036@gmail.com>
2023-03-01 21:07:40 +0300

    update deps

--- build/shared.js
+++ build/shared.js
@@ -1,23 +1,44 @@
+import esbuild from "esbuild"
+import { altvEsbuild } from "altv-esbuild"
+
 const dev = process.argv[2] === "-dev"
 console.log("dev:", dev)
 
-export const esbuildOptions = {
-  bundle: true,
-  format: "esm",
-  target: "esnext",
-  logLevel: "info",
-  watch: dev,
-  sourcemap: dev ? "both" : false,
-  minify: !dev,
-  keepNames: dev,
-}
+export const build = async ({
+  esbuild: esbuildOptions,
+  altvEsbuild: altvEsbuildOptions,
+}) => {
+  const ctx = await esbuild.context({
+    bundle: true,
+    format: "esm",
+    target: "esnext",
+    logLevel: "info",
+    sourcemap: dev ? "both" : false,
+    minify: !dev,
+    keepNames: dev,
+    plugins: [
+      altvEsbuild({
+        dev: {
+          enabled: dev,
+          enhancedRestartCommand: true,
+        },
+        altvEnums: true,
+        bugFixes: {
+          playerDamageOnFirstConnect: true,
+        },
+        
+        ...altvEsbuildOptions,
+      })
+    ],
 
-export const altvEsbuildOptions = {
-  dev: (dev
-    ? {
-      enhancedRestartCommand: true,
-    }
-    : false
-  ),
-  altvEnums: true,
+    ...esbuildOptions,
+  })
+
+  if (dev) {
+    ctx.watch()
+  }
+  else {
+    ctx.rebuild()
+    ctx.dispose()
+  }
 }
