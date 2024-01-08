3e163636c196a7d4dc038be98cf8046f35d5c0a0
xshady <fastik036@gmail.com>
2024-01-09 00:00:56 +0300

    adapt build code for new nodejs

--- build/after-copy.js
+++ build/after-copy.js
@@ -1,3 +1,3 @@
-import { copy } from "./copy"
+import { copy } from "./copy.js"
 
 copy("frontend/dist", "resources/main-view/dist")
--- build/before-copy.js
+++ build/before-copy.js
@@ -1,4 +1,4 @@
-import { copy } from "./copy"
+import { copy } from "./copy.js"
 
 copy("src/resource.toml", "resources/main/resource.toml")
 copy("src/view-resource.toml", "resources/main-view/resource.toml")
